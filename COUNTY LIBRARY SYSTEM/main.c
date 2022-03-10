076179513e3ae7866c8d86c62f8668ca8f74b55f
Adan Abdideq Hassan <97961314+Hassanabdideq@users.noreply.github.com>
2022-03-10 18:04:33 +0300

    Add files via upload

---
+++ COUNTY LIBRARY SYSTEM/main.c
@@ -0,0 +1,23 @@
+/* PROJECT: COUNTY LIBRARY MANAGMENT SYSTEM
+AUTHER : ADAN ABDIDEQ HASSAN
+Compiler:GNU GCC
+License : MIT
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int action;
+    printf("\tCOUNTY LIBRARY MANAGMENT SYSTEM\n");
+    printf("1.Add a new patron\n");
+    printf("2.View patron\n");
+    printf("3.View books\n");
+    printf("4.Add a  new book\n");
+    printf("your action:");
+    scanf("%d",&action);
+
+
+    printf("WELCOME MR. ADAN ABDIDEQ HASSAN");
+    return 0;
+}
